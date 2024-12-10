b4e47e6fd61f69a1cbf06b67d9fa559d04c93357
ryazanov <scooter_zav@mail.ru>
2024-12-10 19:18:29 +0500

    feat(api): add abort signal to getMe

--- src/api/rest/users/me/handler.ts
+++ src/api/rest/users/me/handler.ts
@@ -2,7 +2,7 @@ import { authApiClient } from "@/api/instances/auth";
 import { GET_USERS_ME_PATH } from "./constants";
 import { User } from "./types";
 
-export async function getMe() {
-  const res = await authApiClient.get<User>(GET_USERS_ME_PATH);
+export async function getMe({ signal }: { signal: AbortSignal }) {
+  const res = await authApiClient.get<User>(GET_USERS_ME_PATH, { signal });
   return res.data;
 }
